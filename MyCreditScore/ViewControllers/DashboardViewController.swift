67aead54e49ed637b7ca126df6596d3f2a53f2f4
Keenan Cookson <keenan.cookson@zapper.com>
2021-06-17 13:58:38 +0200

    Text and circular progress bar animate using values from api

--- MyCreditScore/ViewControllers/DashboardViewController.swift
+++ MyCreditScore/ViewControllers/DashboardViewController.swift
@@ -7,29 +7,29 @@
 
 import UIKit
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UIViewController, DataManagerDelegate {
 
     @IBOutlet weak var incrementingCreditScoreLabel: UILabel!
     
     // MARK: - Text animation properties -
     var startValue: Double = 0
-    let endValue: Double = 64
-    let maxCreditScore: Double = 100
+    var endValue: Double = 0
+    var maxCreditScore: Double = 100
     let animationDuration: Double = 2
-    let animationStartDate = Date()
+    var animationStartDate: Date?
     
     // MARK: - Progress bar properties -
     var circularProgressBarView: CircularProgressBarView!
     var creditScorePercentage = CGFloat() // To get from api credit score
     
+    var displayLink: CADisplayLink?
+    
     override func viewDidLoad() {
+        DataManager.shared.delegate = self
         self.creditScorePercentage = CGFloat(endValue)/CGFloat(maxCreditScore)
         super.viewDidLoad()
         setUpCircularProgressBarView()
         
-        // Incrementing Label Code
-        let displayLink = CADisplayLink(target: self, selector: #selector(handleTextUpdate))
-        displayLink.add(to: .main, forMode: .default)
     }
     
     func setUpCircularProgressBarView() {
@@ -41,14 +41,34 @@ class DashboardViewController: UIViewController {
     
     @objc func handleTextUpdate(){
         let now = Date()
+        
+        guard let animationStartDate = animationStartDate else {
+            return
+        }
+        
         let elapsedTime = now.timeIntervalSince(animationStartDate)
         
         if elapsedTime > animationDuration {
             self.incrementingCreditScoreLabel.text = "\(Int(endValue))"
+            if let displayLink = displayLink {
+                displayLink.remove(from: .main, forMode: .default)
+            }
         } else {
             let percentageOfAnimationDuration = elapsedTime / animationDuration
             let incrementingCreditScoreLabelValue = Int(startValue + percentageOfAnimationDuration  * (endValue - startValue))
             self.incrementingCreditScoreLabel.text = "\(incrementingCreditScoreLabelValue)"
+        }
+    }
+    
+    func updateViewController(creditReportInfo: CreditReportInfo?) {
+        self.animationStartDate = Date ()
+        if let endValue = creditReportInfo?.score,
+           let maxCreditScore = creditReportInfo?.maxScoreValue {
+            self.endValue = Double(endValue)
+            self.maxCreditScore = Double(maxCreditScore)
+            viewDidLoad()
+            displayLink = CADisplayLink(target: self, selector: #selector(handleTextUpdate))
+            displayLink?.add(to: .main, forMode: .default)
         }
     }
 }
